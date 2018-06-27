fc8b91aa1f09b8064e32fe505284470816d00268
DESKTOP-C94NL56\patsa <patsa-nan@hotmail.com>
2018-06-27 10:17:24 +0700

    first commit

---
+++ app/src/main/java/com/example/patsa/projectcal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.patsa.projectcal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
